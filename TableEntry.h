9c4f6304a64208eaef088266de5877913d0bb6b4
JSALFER <jsalfer@alumno.upv.es@ldsic-vdi09.upvnet.upv.es>
2023-12-12 18:41:29 +0100

    Añadida implementación de la clase TableEntry

---
+++ TableEntry.h
@@ -0,0 +1,51 @@
+//Declaración e implementación de la clase TableEntry
+
+#ifndef TABLEENTRY_H
+#define TABLEENTRY_H
+
+#include <string> 
+#include <ostream> 
+
+using namespace std;
+
+template <typename V>
+class TableEntry{
+public: 
+  string key; //Clave del par
+  V value; //Valor del par
+  
+public: 
+  TableEntry(string key, V value){ //Método constructor
+    this->key = key;
+    this->value = value;
+  }
+  TableEntry(string key){ //Crea una entrada solo con clave
+    this->key = key;
+  }  
+  TableEntry(){ //Crea una entrada con la clave "" y sin valor
+    key = "";
+  }
+  friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){ //Sobrecarga global del operador == para determinar si dos instancias son iguales
+    if(te1.key == te2.key){
+      return true;
+    }
+    else{ 
+      return false;
+    }
+  }
+  friend bool operator !=(const TableEntry<V> &te1, const TableEntry<V> &te2){ //Sobrecarga global del operador != para determinar si dos instancias son diferentes
+    if(te1.key != te2.key){
+      return true;
+    }
+    else{ 
+      return false;
+    }
+  }
+  friend ostream& operator<<(ostream &out, const TableEntry<V> &te){ //Sobrecarga global del operador << para imprimir el contenido de la entrada por pantalla
+    out << "('" << te.key << "' => " << te.value << ")";
+    return out;
+  }
+};
+
+#endif 
+  
